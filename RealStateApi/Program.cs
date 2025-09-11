d77d68f223fd570388a66c3bd8ec8df7062515c0
Emmanuel Márquez <emmanuelmr1@gmail.com>
2025-09-11 15:06:07 -0500

    feat: prove mongo connection

--- RealStateApi/Program.cs
+++ RealStateApi/Program.cs
@@ -1,6 +1,14 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var mongoClient = new MongoClient("mongodb://localhost:27017");
+var db = mongoClient.GetDatabase("local");
+
+app.MapGet("/", () =>
+{
+    return db.ListCollectionNames().ToList();
+});
 
 app.Run();
